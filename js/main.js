ba2931651ba6076bdafce3e2e568f03f7f8c5b5d
NUHA ALARFAJ <nuhaalarfaj@NUHAs-MacBook-Pro.local>
2018-05-15 16:34:18 -0700

    first

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function timerChange(){
+    var now = new Date();
+
+    now = now.getTime();
+
+    var later = new Date ("May 17, 2018 00:00:01");
+
+    later = later.getTime();
+
+    var difference = later - now;
+
+ var hourUntil =Math.floor((difference % (1000 * 60 * 60 * 24)) / ( 1000 * 60 * 60));
+
+var minUntil = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60 ));
+
+document.getElementById("timer").innerText = "Only" + hourUntil + " hours and " + minUntil +"minuts lift!";
+setTimeout(timerChange , 60000);
+}
+
+timerChange();
+setTimeout(function(){
+    document.getElementById("popup").style.opacity = "1";
+}, 3000)
